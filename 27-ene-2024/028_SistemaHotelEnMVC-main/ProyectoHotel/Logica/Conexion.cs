9c2b2ecf337d83d9530dddfdf75a84c19df6e880
CesarVitalSegura <106455777+CesarVitalSegura@users.noreply.github.com>
2024-01-23 17:24:00 -0600

    27-1-24

---
+++ 27-ene-2024/028_SistemaHotelEnMVC-main/ProyectoHotel/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoHotel.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=DESKTOP-ORF7CB9\\ROOT;Initial Catalog=DB_HOTEL;Integrated Security=True;Encrypt=False";
+    }
+}
